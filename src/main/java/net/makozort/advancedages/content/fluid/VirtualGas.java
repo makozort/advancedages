520bc80897c15a6fd9dc256f8068a11ea49d93ec
DaCodia <Datcoder.hypothermic@gmail.com>
2024-02-24 21:55:49 -0600

    Merge

--- src/main/java/net/makozort/advancedages/content/fluid/VirtualGas.java
+++ src/main/java/net/makozort/advancedages/content/fluid/VirtualGas.java
@@ -3,9 +3,7 @@ package net.makozort.advancedages.content.fluid;
 import com.simibubi.create.content.fluids.VirtualFluid;
 
 public class VirtualGas extends VirtualFluid {
-    public VirtualGas(Properties p) {
-        super(p);
-    }
+    public VirtualGas(Object o) {super((Properties) o);}
 
     //WIP
 }
